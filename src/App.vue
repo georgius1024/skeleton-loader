e7e93e77224888f4dc424bb199ed039dfb25d523
Timofeev Yurii <georgius@ural.ru>
2021-03-08 19:52:44 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="row">
+    <div style="min-width: 340px">
+      <Placeholder type="header" />
+    </div>
+    <div class="col"></div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <Placeholder />
+    </div>
+    <div class="col">
+      <Placeholder />
+    </div>
+    <div class="col">
+      <Placeholder />
+    </div>
+  </div>
+  <div class="row">
+    <div>
+      <Placeholder type="avatar"/>
+    </div>
+    <div class="col">
+      <Placeholder />
+    </div>
+    <div class="col">
+      <Placeholder />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col"></div>
+    <div style="min-width: 140px">
+      <Placeholder type="button" />
+    </div>
+    <div style="min-width: 140px">
+      <Placeholder type="button" />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+//
+import Placeholder from "./components/Placeholder.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Placeholder,
+  },
+};
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.row {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  .col {
+    flex-grow: 1;
+  }
+}
+</style>
---
+++ src/components/Placeholder.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="placeholder" :class="placeholderClass" />
+</template>
+<script>
+export default {
+  name: "Placeholder",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+      validator: (value) => ["text", "header", "button", "avatar", "picture"].includes(value),
+    },
+  },
+  computed: {
+    placeholderClass() {
+      return `placeholder-${this.type}`;
+    },
+  },
+};
+</script>
+<style lang="scss">
+.placeholder {
+  background: rgb(204, 204, 204);
+  position: relative;
+  overflow: hidden;
+  &:after {
+    content: "";
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    // transform: translateX(-100%);
+    background: linear-gradient(
+      90deg,
+      rgba(204, 204, 204, 1) 0%,
+      rgba(151, 151, 151, 0.3) 50%,
+      rgba(204, 204, 204, 1) 100%
+    );
+    animation: placeholder-loading-animation linear 1.5s infinite;
+  }
+  &.placeholder-text {
+    height: 24px;
+    display: block;
+    border-radius: 100px;
+    margin: 0 18px 18px 0;
+  }
+  &.placeholder-header {
+    height: 48px;
+    display: block;
+    border-radius: 100px;
+    margin: 0 0 18px 0;
+  }
+  &.placeholder-button {
+    height: 48px;
+    width: 200px;
+    display: block;
+    border-radius: 2px;
+    margin: 0 18px 18px 0;
+  }
+  &.placeholder-avatar {
+    height: 48px;
+    width: 48px;
+    display: block;
+    border-radius: 100%;
+    margin: 0 18px 18px 0;
+  }
+  &.placeholder-picture {
+    display: block;
+    border-radius: 0;
+    margin: 0 18px 18px 0;
+  }
+
+}
+
+@keyframes placeholder-loading-animation {
+  // 0% {
+  //   transform: translate3d(-100%);
+  // }
+  // 100% {
+  //   transform: translate3d(100%);
+  // }
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
